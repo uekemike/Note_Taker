98db070c29fe3b22b24a8181c2c5a13d0e3b4767
unknown <uekemike@yahoo.com>
2021-09-11 21:34:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -40,18 +40,41 @@ const readFromFile = util.promisify(fs.readFile);
 */
 
 const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) =>{
+    fs.readFile(file, 'utf8', (err, data) => {
         if(err){
             console.error(err);
         }else {
-            const parseData = JSON.parse(data);
-            parseData.push(content);
-            writeToFile(file, parseData);
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
         }
     });
 };
 
 //Get route for retrieving all the notes
-app.get("/api/notes", (req, res) =>
-console.info(`${req.method} request received for notes`);
-readFromFile())
+app.get("/api/notes", (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+    console.info("Read db.json")
+});
+
+//Create new notes
+app.post("/api/notes",(req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    const { title, text } = req.body;
+    if(req.body){
+        const newNote = {
+            title,
+            text,
+            id: uuid()
+        };
+
+        readAndAppend(newNote, "./db/db.json")
+        res.json(`Note added successfully `);
+    } else {
+        res.json("error while adding Notes")
+    }
+});
+
+//Delete the note: for deleting the note, we need to specify the d of the note which we are going to delete
+
